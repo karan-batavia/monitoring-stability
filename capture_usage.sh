3b5dbb1eab34af3cf83a15fca5023ff13517b349
karan-batavia <karan-batavia@privado.ai>
2022-11-25 11:09:26 +0530

    path for report file changed

--- capture_usage.sh
+++ capture_usage.sh
@@ -1,9 +1,8 @@
 while true; 
 do 
-top -b -n1 > output.txt;
-cat output.txt | grep -a '%Cpu(s)' >> results_1.txt;
-cat output.txt | grep -a 'MiB Mem' >> results_1.txt;
-echo "\n" >> results_1.txt;
+top -b -n1 > ${GITHUB_WORKSPACE}/tools/output.txt;
+cat output.txt | grep -a '%Cpu(s)' >> ${GITHUB_WORKSPACE}/tools/results_1.txt;
+cat output.txt | grep -a 'MiB Mem' >> ${GITHUB_WORKSPACE}/tools/results_1.txt;
 sleep 60;  # time after which to run the command
 done
 
